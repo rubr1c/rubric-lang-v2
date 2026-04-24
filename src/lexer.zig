19f783628fe60612c36226fa5a8823dae1ab3283
rubr1c <alizaghloul64@gmail.com>
2026-04-24 16:35:53 +0300

    im testing way too much i need to refactor

--- src/lexer.zig
+++ src/lexer.zig
@@ -5,8 +5,9 @@ const Token = union(enum) {
     identifier: []const u8,
     assignment: void,
 
-    // datatypes
+    // types
     integer32: i32,
+    integer32_id: void, 
 
     // ops
     add: void,
@@ -18,6 +19,8 @@ const Token = union(enum) {
 
     comparison: void,
 
+    colon: void,
+
     // misc
     EOF: void,
 };
@@ -26,10 +29,22 @@ const LexerError = error{
     Expected,
 };
 
+const IDENTIFIER = "let";
+const I32_ID = "int32";
+
 pub const Tokenizer = struct {
     src: []const u8,
     pos: usize = 0,
     allocator: std.mem.Allocator,
+    tokens: std.ArrayList(Token),
+
+    pub fn init(allocator: std.mem.Allocator, src: []const u8) !Tokenizer {
+        return .{
+            .tokens = try std.ArrayList(Token).initCapacity(allocator, 1024),
+            .allocator = allocator,
+            .src = src,
+        };
+    }
 
     pub inline fn empty(self: *const Tokenizer) bool {
         return self.pos >= self.src.len;
@@ -40,18 +55,14 @@ pub const Tokenizer = struct {
         return self.src[self.pos + offset];
     }
 
-    pub inline fn next_tok(self: *Tokenizer) ?Token {
+    pub inline fn add_tok(self: *Tokenizer, token: Token) !void {
+        return self.tokens.append(self.allocator, token);
+    }
+
+    pub inline fn next_tok(self: *Tokenizer) !bool {
         self.skip_space();
 
-        if (self.empty()) return null;
-
-        if (std.mem.startsWith(u8, self.src[self.pos..], "let")) {
-            if (self.peek(3)) |next| {
-                if (next != ' ') return null;
-            }
-            self.pos += 3;
-            return .initalizer;
-        }
+        if (self.empty()) return false;
 
         const first = self.src[self.pos];
 
@@ -60,42 +71,104 @@ pub const Tokenizer = struct {
                 if (self.peek(1)) |next| {
                     if (next == '+') {
                         self.pos += 2;
-                        return .increment;
+                        try self.add_tok(.increment);
+                        return true;
                     }
                 }
                 self.pos += 1;
-                return .add;
+                try self.add_tok(.add);
+                return true;
             },
             '-' => {
                 if (self.peek(1)) |next| {
                     if (next == '-') {
                         self.pos += 2;
-                        return .decrement;
+                        try self.add_tok(.decrement);
+                        return true;
                     }
                 }
                 self.pos += 1;
-                return .sub;
+                try self.add_tok(.sub);
+                return true;
             },
             '*' => {
                 self.pos += 1;
-                return .mul;
+                try self.add_tok(.mul);
+                return true;
             },
             '/' => {
                 self.pos += 1;
-                return .div;
+                try self.add_tok(.div);
+                return true;
             },
             '=' => {
                 if (self.peek(1)) |next| {
                     if (next == '=') {
-                        self.pos += 21;
-                        return .comparison;
+                        self.pos += 2;
+                        try self.add_tok(.comparison);
+                        return true;
                     }
                 }
                 self.pos += 1;
-                return .assignment;
+                try self.add_tok(.assignment);
+                return true;
             },
-            else => null,
+            else => self.lex_id(),
         };
+    }
+
+    // very messy just testin
+    inline fn lex_id(self: *Tokenizer) !bool {
+        if (std.mem.startsWith(u8, self.src[self.pos..], IDENTIFIER)) {
+            const next = self.peek(IDENTIFIER.len);
+            if (next == null or std.ascii.isWhitespace(next.?)) {
+                self.pos += IDENTIFIER.len;
+                try self.add_tok(.initalizer);
+                self.skip_space();
+
+                if (self.empty()) return LexerError.Expected;
+
+                var buff = try std.ArrayList(u8).initCapacity(self.allocator, 256);
+
+                while (!self.empty() and 
+                         self.src[self.pos] != ' ' and self.src[self.pos] != ':')
+                 {
+                    try buff.append(self.allocator, self.src[self.pos]);
+                    self.pos += 1;
+                }
+
+                if (buff.items.len == 0) return LexerError.Expected;
+
+                try self.add_tok(.{ .identifier = try buff.toOwnedSlice(self.allocator) });
+                
+                if (self.empty()) return true;
+
+                self.skip_space();
+
+                if (self.src[self.pos] == ':') {
+                    try self.add_tok(.colon);
+                    self.pos += 1;
+
+                    self.skip_space();
+
+                    var t_buff = try std.ArrayList(u8).initCapacity(self.allocator, 256);
+                    var count: usize = 0;
+                    while (self.pos + count < self.src.len and self.src[self.pos + count] != ' ') {
+                        try t_buff.append(self.allocator, self.src[self.pos + count]);
+                        count += 1;
+                    }
+
+                    if (std.mem.eql(u8, t_buff.items, I32_ID)) {
+                        try self.add_tok(.integer32_id);
+                        self.pos += count;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        return false;
     }
 
     inline fn skip_space(self: *Tokenizer) void {
@@ -106,40 +179,8 @@ pub const Tokenizer = struct {
         }
     }
 
-    inline fn expect(self: *Tokenizer, token: Token) !?Token {
-        self.skip_space();
-        if (self.empty()) return null;
-
-        return switch (token) {
-            .identifier => {
-                var buff = try std.ArrayList(u8).initCapacity(self.allocator, 256);
-
-                while (!self.empty() and self.src[self.pos] != ' ') {
-                    try buff.append(self.allocator, self.src[self.pos]);
-                    self.pos += 1;
-                }
-                return Token{ .identifier = try buff.toOwnedSlice(self.allocator) };
-            },
-            else => null,
-        };
-    }
-
-    pub fn build(self: *Tokenizer) !std.ArrayList(Token) {
-        var tokens = try std.ArrayList(Token).initCapacity(self.allocator, 1024);
-
-        while (self.next_tok()) |token| {
-            try tokens.append(self.allocator, token);
-            if (token == .initalizer) {
-                if (try self.expect(.{ .identifier = "" })) |id| {
-                    try tokens.append(self.allocator, id);
-                } else {
-                    return LexerError.Expected;
-                }
-            }
-        }
-
-        try tokens.append(self.allocator, .EOF);
-
-        return tokens;
+    pub fn build(self: *Tokenizer) !void {
+        while (try self.next_tok()) {}
+        try self.tokens.append(self.allocator, .EOF);
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,14 @@
 const std = @import("std");
 const lexer = @import("lexer.zig");
 
-pub fn main() void {
+pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
 
     const allocator = arena.allocator();
 
-    var tokenizer: lexer.Tokenizer = .{
-        .src = "let cold ++--=/*==",
-        .allocator = allocator,
-    };
+    var tokenizer: lexer.Tokenizer = try .init(allocator, "let cold : int32 ++/*=--==");
+    try tokenizer.build();
 
-    const tokens = tokenizer.build();
-
-    std.debug.print("{any}\n", .{tokens});
+    std.debug.print("{any}\n", .{tokenizer.tokens.items});
 }
