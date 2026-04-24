45adc6d75ad54423b98f15c9c38c01e123c4d57d
rubr1c <alizaghloul64@gmail.com>
2026-04-25 01:54:35 +0300

    feat: add fn to check num types
    some optimizations should be done here but it works

--- src/lexer.zig
+++ src/lexer.zig
@@ -9,6 +9,9 @@ const Token = union(enum) {
     int: i64,
     float: f64,
     integer32_id: void,
+    float32_id: void,
+    integer64_id: void,
+    float64_id: void,
 
     // ops
     add: void,
@@ -33,7 +36,8 @@ const LexerError = error{
 };
 
 const INITALIZER = "let";
-const I32_ID = "int32";
+const INT_ID = "int";
+const FLOAT_ID = "float";
 
 pub const Tokenizer = struct {
     src: []const u8,
@@ -128,39 +132,56 @@ pub const Tokenizer = struct {
     inline fn lex_id(self: *Tokenizer) !void {
         var buff = try std.ArrayList(u8).initCapacity(self.allocator, 256);
         if (std.mem.startsWith(u8, self.src[self.pos..], INITALIZER)) {
-           try self.read_lex_id(INITALIZER, .initalizer); 
-        } else if (std.mem.startsWith(u8, self.src[self.pos..], I32_ID)) {
-            try self.read_lex_id(I32_ID, .integer32_id);
+            try self.read_lex_id(INITALIZER, .initalizer);
         } else if (std.ascii.isDigit(self.src[self.pos])) {
             const is_float = try self.read_num(&buff);
             if (is_float) {
-                try self.add_tok(
-                    .{ .float = try std.fmt.parseFloat(
-                        f64, try buff.toOwnedSlice(self.allocator)
-                    ) }
-                );
+                try self.add_tok(.{ .float = try std.fmt.parseFloat(f64, try buff.toOwnedSlice(self.allocator)) });
             } else {
-                try self.add_tok(
-                    .{ .int = try std.fmt.parseInt(
-                        i64, try buff.toOwnedSlice(self.allocator), 10
-                    ) }
-                );
+                try self.add_tok(.{ .int = try std.fmt.parseInt(i64, try buff.toOwnedSlice(self.allocator), 10) });
             }
-        } else {
+            // self.read_id() is called twice and should be optimized.
+        } else if ((try self.read_num_id(&buff)) != null) {} else {
             try self.read_id(&buff);
             try self.add_tok(.{ .identifier = try buff.toOwnedSlice(self.allocator) });
+        }
+    }
+
+    inline fn read_num_id(
+        self: *Tokenizer,
+        buff: *std.ArrayList(u8),
+    ) !?void {
+        try self.read_id(buff);
+        var start: usize = 0;
+        var is_float = false;
+
+        if (std.mem.startsWith(u8, buff.items, INT_ID)) {
+            start = INT_ID.len;
+        } else if (std.mem.startsWith(u8, buff.items, FLOAT_ID)) {
+            start = FLOAT_ID.len;
+            is_float = true;
+        } else {
+            return null;
+        }
+
+        const size = buff.items[start..];
+
+        if (std.mem.startsWith(u8, size, "32")) {
+            try self.add_tok(if (is_float) .float32_id else .integer32_id);
+        } else if (std.mem.startsWith(u8, size, "64")) {
+            try self.add_tok(if (is_float) .float64_id else .integer64_id);
+        } else {
+            return null;
         }
     }
 
     // returns true if float
     inline fn read_num(self: *Tokenizer, buff: *std.ArrayList(u8)) !bool {
         var is_float = false;
-        while (!self.empty() and (
-                std.ascii.isDigit(self.src[self.pos]) or self.src[self.pos] == '.')
-        ) {
+        while (!self.empty() and (std.ascii.isDigit(self.src[self.pos]) or self.src[self.pos] == '.')) {
             const has_point = self.src[self.pos] == '.';
             if (is_float and has_point) return LexerError.InvalidNum;
-            if (has_point) is_float = true; 
+            if (has_point) is_float = true;
 
             try buff.append(self.allocator, self.src[self.pos]);
             self.pos += 1;
--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,8 @@ pub fn main() !void {
 
     const allocator = arena.allocator();
 
-    var tokenizer: lexer.Tokenizer = try .init(allocator, "let cold : int32 = 20;");
+    // colon for types have to have a space before the id prob should fix.
+    var tokenizer: lexer.Tokenizer = try .init(allocator, "let cold : float32 = 20.9;");
     try tokenizer.build();
 
     std.debug.print("{any}\n", .{tokenizer.tokens.items});
